6a69a5404b83fa783c311980ba076d07b2efa9b8
Bill Muxworthy <bill.muxworthy@doctorcareanywhere.com>
2022-02-14 16:41:18 +0000

    Add user repository

---
+++ bindy-street-tech-test/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bindy_street_tech_test.Repositories
+{
+    public class UserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
